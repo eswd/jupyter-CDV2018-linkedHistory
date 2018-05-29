7b55ce20a4f65ff3b81d2798d6dd83e637753b89
MrWu <eswd@wustig.de>
2018-05-29 13:49:23 +0200

    Company type added

--- expand_csv.py
+++ expand_csv.py
@@ -3,12 +3,34 @@ import csv
 
 CSV_FILE = 'csv.csv'
 NEW_CSV_FILE = 'csv_new.csv'
+COMPANY_TYPES = [
+'Musikalienbuchhandlung',
+'Verlagsbuchhandlung',
+'Buchhandlung',
+'Verlag',
+'Antiquariat',
+'Verlagsbuchhandlung'
+]
 
 
 def extract_city(titel):
     
-    city=titel.split(',')[-1]
+    city=titel.split(',')[-1].strip()
     return city
+
+def company_type(description):
+    """
+    Detect the kind of company
+    """
+    type = []
+    import pdb;pdb.set_trace
+
+    type = [i for i in COMPANY_TYPES if i in description]
+
+    if not type:
+        type.append('nicht erkannt')
+    
+    return type
 
 
 def new_csv():
@@ -23,15 +45,20 @@ def new_csv():
 
             fieldnames = csv_data.fieldnames
             fieldnames.append('Stadt')
+            fieldnames.append('Firmentyp')
 
-            #crate a new csv_file
+            #crate a new csv_file with new columns
             new_csv = csv.DictWriter(f_new, fieldnames=fieldnames)
             new_csv.writeheader()
 
-            # exctract and append citys and write the new row
+            # write new  rows
             for i in csv_data:
                 i['Stadt'] = extract_city(i['Aktentitel'])
+                i['Firmentyp'] = company_type(i['Aktentitel'])
+
                 new_csv.writerow(i)
+
+
 
 if __name__ == '__main__':
     new_csv()
